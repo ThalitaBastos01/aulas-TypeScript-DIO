02d3f94fc02332fe5297ce502e3edbef2a50d5e2
Thalita coelho <thalita.bastos94@gmail.com>
2024-06-15 22:46:39 -0300

    .

--- app.ts
+++ app.ts
@@ -1,5 +1,12 @@
-const soma = (a: number, b: number) => {
-    console.log(a + b);
-}
+// const soma = (a: number, b: number) => {
+//     console.log(a + b);
+// }
 
-soma(1, 2)
+// soma(1, 2)
+// soma(2, 8)
+// aula 2 
+
+var a = 'a' // escopo global 
+let b = 'b' // eu posso alterar o valor dessa variavel
+
+const c = '2' // é uma variavel imutavel
