89091a10a2ba6676e889ba08f949caf2d9273c14
Thalita coelho <thalita.bastos94@gmail.com>
2024-06-19 15:02:55 -0300

    objetos e interfaces

--- app.ts
+++ app.ts
@@ -10,3 +10,20 @@ var a = 'a' // escopo global
 let b = 'b' // eu posso alterar o valor dessa variavel
 
 const c = '2' // é uma variavel imutavel
+
+///////////////////////////////////////////////////////////////////////////
+
+let m: any = 2 // o conceito eny recebe todos os tipos, mas não é orientado sair usando em tudo 
+
+///////////////////////////////////////////
+// objetos e interfaces 
+
+interface Pessoa {
+    nome: string,
+    idade: number
+}
+
+const pessoa: Pessoa = {
+    nome: 'thalita',
+    idade: 25
+}
